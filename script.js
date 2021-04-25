b0450bad6fae40cda052c09f2cf2a224b5e7addd
Panos1987 <64789902+Panos1987@users.noreply.github.com>
2021-04-25 15:52:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,20 @@ var year = new Date().getFullYear()+parseInt(years);
 
 function principal_validation() 
 {
-  if (principal <= "0"); 
-     {alert("Enter a positive number");
-     ("principal").focus()};
-}; 
+  if (principal <= "0") {
+      alert("Enter a positive number");
+      ("principal").focus()
+  }
+} 
 
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
-};
+}
 
 function compute()
 {
-    document.innerHTML = "If you deposit " + principal + " at an interest rate of " + rate + ", you will receive an amount of interest " + interest + " in the year " + year + ".";
-};
+    var answer = document.getElementById("answer").innerHTML = "If you deposit " + principal + " at an interest rate of " + rate + ", you will receive an amount of interest " + interest + " in the year " + year + ".";
+}
 
